0954389d8124e0fa5f4faf99f16c60ca530fa379
aleksandr_markhel <volatpy@gmail.com>
2023-06-13 21:09:40 +0300

    debugging finish

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,25 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
+//////////////////////////////////////////////////////
+// Coding Challenge #1
+const tempsOne = [17, 21, 23];
+const tempsTwo = [12, 5, -5, 30, 4];
+
+const printForecast = function (arr) {
+	let report = "...";
+	let maxTemp = arr[0];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > maxTemp) maxTemp = arr[i];
+		report += `${maxTemp}°С in ${i + 1} days...`;
+	}
+	return report;	
+}
+
+console.log(printForecast(tempsTwo));
+
+
+
 
 
 
